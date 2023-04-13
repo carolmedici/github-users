93d11208bf46c5ffc396ea0272ffc556695762fa
carolmedici <carolina.obstetriz@gmail.com>
2023-04-12 22:40:22 -0300

    :bricks: start structure

---
+++ src/Components/HomePage/index.jsx
@@ -0,0 +1,12 @@
+
+
+const HomePage = () =>{
+    return(
+        <>
+        <h1>DevHunt</h1>
+        <input type="text" placeholder="Enter a GitHub username" />
+        </>
+    )
+}
+
+export default HomePage
